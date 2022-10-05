58d65a2dbc86c1aa54ef1515289d61947949b048
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-05 17:50:37 +0530

    Create inputoutputstring.java
    Here, We declare and print string in java

---
+++ Day6/inputoutputstring.java
@@ -0,0 +1,21 @@
+//String
+//Declaring and Printing
+
+import java.util.Scanner;
+
+public class Day6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //Declaring a string
+        String str="Hello there";
+        String str2=new String();
+        System.out.println("Enter other String");
+        sc.nextLine();
+        //Printing a String
+        System.out.println(str);
+        System.out.println(str2);
+
+
+
+    }
+}
