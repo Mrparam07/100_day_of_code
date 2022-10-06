feb8f2b7b3f58a3d2328662af697adfbfee4619b
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-06 20:54:15 +0530

    MergingTwoArrays

---
+++ Day7/MergetwoArray.java
@@ -0,0 +1,29 @@
+//Smallest and largest element in 1DArray
+
+
+import java.security.Key;
+import java.util.*;
+public class Day7_2 {
+    public static void main(String[] args) {
+        int arr[]={2,3,4,5,6,1,7,8,9};
+        int arr2[]={13,32,56,23,68,87,76,78,90};
+        int arrSum[]=new int[9]; 
+
+
+        for (int i = 0; i <arrSum.length; i++)
+        {
+            arrSum[i]=arr[i]+arr2[i]; //Sum of array 1 and array 2 in array3
+
+        }
+        for (int i = 0; i <arrSum.length; i++)
+        {
+            System.out.print(arrSum[i]+" "); //printing third array
+
+        }
+
+    }
+}
+/*
+Output
+15 35 60 28 74 88 83 86 99 
+*/
