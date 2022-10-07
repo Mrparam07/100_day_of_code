8e51b4392c08ae539179ab628123fe377985562b
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-07 15:24:28 +0530

    subStringSolution

---
+++ Day8/subString.java
@@ -0,0 +1,54 @@
+/*
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find
+Returns
+
+string: the string ' + "\n" + ' where and are the two substrings
+Input Format
+
+The first line contains a string denoting .
+The second line contains an integer denoting .
+
+Constraints
+
+ consists of English alphabetic letters only (i.e., [a-zA-Z]).
+Sample Input 0
+
+welcometojava
+3
+Sample Output 0
+
+ava
+wel
+*/
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        java.util.List<String> a = new java.util.ArrayList<>();
+
+        for(int i=0;i<s.length()-k+1;i++){
+                a.add(s.substring(i,i+k));   
+        }
+        java.util.Collections.sort(a);
+         smallest = a.get(0);
+         largest = a.get(a.size()-1);
+        
+        return smallest + "\n" + largest;
+    }
+
