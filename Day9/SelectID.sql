e3616131421a9be71089d2e0a8eb05f0c8cfe908
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-08 19:42:11 +0530

    SelectingEntitiesbyID

---
+++ Day9/SelectID.sql
@@ -0,0 +1,9 @@
+/*
+Query all columns for a city in CITY with the ID 1661.
+The CITY table is described as follows:
+https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+Expected Output
+1661 Sayama JPN Saitama 162472
+*/
+SELECT * FROM CITY
+WHERE ID=1661;
