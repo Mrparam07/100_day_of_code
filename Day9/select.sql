60fa0509cc46edf2824129e5e0021e6c7a4c4e54
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-08 19:41:01 +0530

    SelectingAllEntities

---
+++ Day9/select.sql
@@ -0,0 +1,16 @@
+/*
+Query all columns (attributes) for every row in the CITY table.
+The CITY table is described as follows:
+https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+Expected Output
+6 Rotterdam NLD Zuid-Holland 593321 
+3878 Scottsdale USA Arizona 202705 
+3965 Corona USA California 124966 
+3973 Concord USA California 121780 
+3977 Cedar Rapids USA Iowa 120758 
+3982 Coral Springs USA Florida 117549 
+4054 Fairfield USA California 92256 
+4058 Boulder USA Colorado 91238 
+4061 Fall River USA Massachusetts 90555 
+*/
+SELECT * FROM CITY;
