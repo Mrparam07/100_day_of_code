f4224e395c540d2daaabcae1b9331667f354dcbb
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-08 19:49:33 +0530

    StationSolution3

---
+++ Day9/StationSolution3.sql
@@ -0,0 +1,244 @@
+SELECT DISTINCT CITY FROM STATION
+WHERE MOD(ID,2) = 0;
+
+/*
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+The STATION table is described as follows:
+https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true
+Expected  Output (stdout)
+Aguanga 
+Alba 
+Albany 
+Amo 
+Andersonville 
+Archie 
+Athens 
+Atlantic Mine 
+Bainbridge 
+Baker 
+Bass Harbor 
+Bayville 
+Beaufort 
+Bellevue 
+Benedict 
+Bennington 
+Bentonville 
+Biggsville 
+Bison 
+Bono 
+Bowdon 
+Bridgton 
+Brownsdale 
+Brownstown 
+Bullhead City 
+Busby 
+Cahone 
+Calhoun 
+Cannonsburg 
+Canton 
+Carver 
+Centertown 
+Cherry 
+Cheswold 
+Chignik Lagoon 
+Childs 
+Chilhowee 
+Chokio 
+Church Creek 
+Clancy 
+Climax 
+Clio 
+Clovis 
+Clutier 
+Coaling 
+Cobalt 
+Compton 
+Crane Lake 
+Cromwell 
+Crouseville 
+Curdsville 
+Daleville 
+Decatur 
+Deep River 
+Delano 
+Delray Beach 
+Delta 
+Dryden 
+Dumont 
+Dundee 
+East Haddam 
+East Irvine 
+Eastlake 
+Effingham 
+Elkton 
+Elm Grove 
+Ermine 
+Eros 
+Eskridge 
+Eufaula 
+Eustis 
+Everton 
+Farmington 
+Firebrick 
+Five Points 
+Forest 
+Forest Lakes 
+Fort Lupton 
+Frankfort Heights 
+Franklin 
+Gales Ferry 
+Garden City 
+Garland 
+Glen Carbon 
+Glencoe 
+Gorham 
+Grand Terrace 
+Grandville 
+Grapevine 
+Grayslake 
+Greens Fork 
+Greenview 
+Greenway 
+Grosse Pointe 
+Gustine 
+Hackleburg 
+Hampden 
+Hanscom Afb 
+Harmony 
+Haverhill 
+Hayfork 
+Hayneville 
+Healdsburg 
+Henderson 
+Heyburn 
+Holbrook 
+Hope 
+Howard Lake 
+Jolon 
+Julian 
+Kanorado 
+Kell 
+Kenner 
+Keyes 
+Kirkland 
+Kirksville 
+Kismet 
+Kissee Mills 
+Knobel 
+Larkspur 
+Leakesville 
+Leavenworth 
+Ledyard 
+Lee 
+Lismore 
+Little Rock 
+Loma Mar 
+Lottie 
+Ludington 
+Ludlow 
+Lupton 
+Lynnville 
+Macy 
+Magnolia 
+Manchester 
+Many 
+Marine On Saint Croix 
+Marion Junction 
+Mc Henry 
+Mcbrides 
+Mccomb 
+Melber 
+Mesick 
+Mid Florida 
+Middleboro 
+Millville 
+Mineral Point 
+Monroe 
+Montgomery City 
+Montrose 
+Morenci 
+Mosca 
+Mullan 
+Napoleon 
+Neon 
+New Ross 
+Newton Center 
+Norphlet 
+Norris 
+Norris City 
+North Berwick 
+North Monmouth 
+Norvell 
+Norwood 
+Oakfield 
+Oconee 
+Onaway 
+Orange City 
+Orange Park 
+Osage City 
+Ottertail 
+Ozona 
+Palatka 
+Palm Desert 
+Paron 
+Pattonsburg 
+Peachtree City 
+Pheba 
+Philipsburg 
+Pico Rivera 
+Pine Bluff 
+Pine City 
+Pleasant Grove 
+Pony 
+Prairie Du Rocher 
+Prince Frederick 
+Randall 
+Rantoul 
+Ravenden Springs 
+Raymondville 
+Regina 
+Richland 
+Robertsdale 
+Rocheport 
+Rockton 
+Rogers 
+Round Pond 
+Roy 
+Rumsey 
+Rydal 
+Saint Paul 
+Saint Petersburg 
+Salem 
+Schleswig 
+Seward 
+Sherrill 
+Shingletown 
+Shreveport 
+Siler 
+Sizerock 
+Skanee 
+South Britain 
+South Haven 
+Southport 
+Strasburg 
+Sturdivant 
+Taft 
+Tarzana 
+Tipton 
+Turners Falls 
+Ukiah 
+Vulcan 
+Walnut 
+Watkins 
+West Baden Springs 
+West Hills 
+West Hyannisport 
+White Horse Beach 
+Wickliffe 
+Winter Park 
+Yalaha 
+Yazoo City 
+Yellow Pine 
+Yuma 
+Zachary 
+*/
