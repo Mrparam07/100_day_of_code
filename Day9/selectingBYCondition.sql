8854ed7a169ec56ea8d8b65649b3e9340708d077
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-08 19:43:04 +0530

    Create selectingBYCondition.sql

---
+++ Day9/selectingBYCondition.sql
@@ -0,0 +1,14 @@
+/*Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+The CITY table is described as follows:
+https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+Expected Output
+3878 Scottsdale USA Arizona 202705 
+3965 Corona USA California 124966 
+3973 Concord USA California 121780 
+3977 Cedar Rapids USA Iowa 120758 
+3982 Coral Springs USA Florida 117549 
+*/
+
+SELECT * FROM CITY
+WHERE POPULATION >100000
+AND COUNTRYCODE="USA";
