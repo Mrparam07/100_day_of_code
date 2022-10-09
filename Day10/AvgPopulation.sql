239616d8df4f6b9f00fb9eaa03f6823ec186ffba
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-09 22:26:15 +0530

    AveragePopulation

---
+++ Day10/AvgPopulation.sql
@@ -0,0 +1,9 @@
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+Input Format
+The CITY table is described as follows:
+https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+Expected Output
+454250
+*/
+SELECT AVG(POPULATION) FROM CITY;
