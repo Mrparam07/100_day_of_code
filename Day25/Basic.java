63e26e64dd8e3a1b3957560a2d270bdf83c998b2
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-24 23:19:41 +0530

    Create Basic.java

---
+++ Day25/Basic.java
@@ -0,0 +1,18 @@
+
+class Student
+{ 
+    int id;//data member (also instance variable) 
+    String name; //data member (also instance variable) 
+ 
+    public static void main(String args[])
+    { 
+        Student s1=new Student();//creating an object of Student 
+        System.out.println(s1.id); 
+        System.out.println(s1.name); 
+     } 
+} 
+
+//OUTPUT
+
+0
+null
