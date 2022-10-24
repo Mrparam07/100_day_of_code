d8890b9a4fac162af3fccf0b9debb90813097bdc
Mrparam07 <114753610+Mrparam07@users.noreply.github.com>
2022-10-24 23:25:55 +0530

    Create MultipleObjects.java

---
+++ Day25/MultipleObjects.java
@@ -0,0 +1,36 @@
+//CREATING OBJECT
+
+public class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
+
+/*OUTPUT
+
+5
+
+*/
+
+//CREATING MULTIPLE OBJECT
+
+public class Real {
+  int x = 10;
+
+  public static void main(String[] args) {
+    Real ob1 = new Real();  // Object 1
+    Real ob2 = new Real();  // Object 2
+    System.out.println(ob1.x);
+    System.out.println(ob2.(x+5));
+  }
+}
+
+/*OUTPUT
+
+10
+15
+
+*/
