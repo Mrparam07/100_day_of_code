ad0fdb7df2aadb070539b340a8cd2f42eab654da
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-10-27 22:55:09 +0530

    Create MethodOverriding.java

---
+++ Day28/MethodOverriding.java
@@ -0,0 +1,37 @@
+class ProgrammingLanguage {
+  public void display() {
+    System.out.println("I am Programming Language.");
+  }
+}
+
+class Java extends ProgrammingLanguage {
+  @Override
+  public void display() {
+    System.out.println("I am Object-Oriented Programming Language.");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+
+    // declare an object variable
+    ProgrammingLanguage pl;
+
+    // create object of ProgrammingLanguage
+    pl = new ProgrammingLanguage();
+    pl.display();
+
+    // create object of Java class
+    pl = new Java();
+    pl.display();
+  }
+}
+
+/*
+
+OUTPUT
+
+I am Programming Language.
+I am Object-Oriented Programming Language.
+
+*/
