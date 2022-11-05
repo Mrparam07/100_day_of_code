2bf5f4bd44c01302fc9c7c27f5591b7963b1d510
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-11-05 19:37:18 +0530

    Create TryCatch.java

---
+++ Day37/TryCatch.java
@@ -0,0 +1,22 @@
+public class TryCatchExample9 {  
+  
+    public static void main(String[] args) {  
+        try  
+        {  
+        int arr[]= {1,3,5,7};  
+        System.out.println(arr[10]); //throw exception   
+        }  
+            // handling the array exception  
+        catch(ArrayIndexOutOfBoundsException e)  
+        {  
+            System.out.println(e);  
+        }  
+        System.out.println("Next Block of code");  
+    }  
+      
+}  
+
+//OUTPUT
+
+//java.lang.ArrayIndexOutOfBoundsException: 10
+//rest of the code
