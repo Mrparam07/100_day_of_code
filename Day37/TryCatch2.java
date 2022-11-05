fd705e32deb5b5a8a3dea1e7ebc95117dddf58d9
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-11-05 19:47:55 +0530

    Create TryCatch2.java

---
+++ Day37/TryCatch2.java
@@ -0,0 +1,22 @@
+
+public class TryCatch {  
+  
+    public static void main(String[] args) {  
+          
+        try  
+        {  
+        int data1=50/0; //may throw exception   
+  
+        }  
+             // handling the exception  
+        catch(Exception e)  
+        {  
+            // generating the exception in catch block  
+        int data2=50/0; //may throw exception   
+  
+        }  
+    System.out.println("rest of the code");  
+    }  
+}  
+
+//Exception in thread "main" java.lang.ArithmeticException: / by zero
