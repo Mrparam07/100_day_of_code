1ec533c5493f9247cc81370b430ac444f44ac0f9
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-11-15 17:35:41 +0530

    Create ThrowAndThrows.java

---
+++ Day47/ThrowAndThrows.java
@@ -0,0 +1,29 @@
+public class Ex9 {
+}
+ class TestThrowAndThrows
+{
+    // defining a user-defined method
+    // which throws ArithmeticException
+    static void method() throws ArithmeticException
+    {
+        System.out.println("Inside the method()");
+        throw new ArithmeticException("throwing ArithmeticException");
+    }
+    //main method
+    public static void main(String args[])
+    {
+        try
+        {
+            method();
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("caught in main() method");
+        }
+    }
+}
+
+/*
+Inside the method()
+caught in main() method
+*/
