38714b6747b8e101d8f2681d1cd327d461801620
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-11-26 23:29:56 +0530

    Create MaxSubArraySum.java

---
+++ Day58/MaxSubArraySum.java
@@ -0,0 +1,69 @@
+/*
+Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {1,2,3,-2,5}
+Output:
+9
+Explanation:
+Max subarray sum is 9
+of elements (1, 2, 3, -2, 5) which 
+is a contiguous subarray.
+Example 2:
+
+Input:
+N = 4
+Arr[] = {-1,-2,-3,-4}
+Output:
+-1
+Explanation:
+Max subarray sum is -1 
+of element (-1)
+
+Your Task:
+You don't need to read input or print anything. The task is to complete the function maxSubarraySum() which takes Arr[] and N as input parameters and returns the sum of subarray with maximum sum.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 106
+-107 ≤ A[i] ≤ 107
+*/
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        
+        long sum=0;
+        long max=Integer.MIN_VALUE;
+        
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+            
+            if(sum>max)
+            max=sum;
+        
+            if(sum<0)
+            sum=0;
+        }
+        return max;
+    }
+    
+}
+/*
+"C:\Users\PARAMVEER SINGH\.jdks\openjdk-18.0.2.1-1\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.2.1\lib\idea_rt.jar=56401:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.2.1\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath D:\IdeaProjects\DDelta\out\production\CodingNinjaas MaxSubArraySum
+Maximum Sum::34
+
+Process finished with exit code 0
+
+*/
