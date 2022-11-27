38634b7992cb31b7ffcbdc499660fd4e1d7ab4b4
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-11-27 23:57:56 +0530

    Update BubbleSort.java

--- Day59/BubbleSort.java
+++ Day59/BubbleSort.java
@@ -1,1 +1,38 @@
-//
+//Bubble Sort in Java
+public class BubbleSort {
+        public static void bubbleSort(int arr[])
+        {
+           int n = arr.length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1]) {
+                        // swap arr[j+1] and arr[j]
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+    public static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+      //  BubbleSort ob = new BubbleSort();
+        int arr[] = { 5, 1, 4, 2, 8 };
+        bubbleSort(arr);
+        System.out.println("Sorted array");
+        printArray(arr);
+    }
+}
+/*
+"C:\Users\PARAMVEER SINGH\.jdks\openjdk-18.0.2.1-1\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.2.1\lib\idea_rt.jar=60139:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.2.1\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath D:\IdeaProjects\DDelta\out\production\DDelta BubbleSort
+Sorted array
+1 2 4 5 8 
+
+Process finished with exit code 0
+
+*/
